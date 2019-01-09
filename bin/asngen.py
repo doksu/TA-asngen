fc110566957c3dba00ece1bc93b01d11f4151a0e
Trusted Subject <trustedsubject@gmail.com>
2019-01-09 14:34:35 +1000

    Applied nicolas-rrr's patch from https://github.com/doksu/TA-asngen/issues/5#issuecomment-452367423 to support new MaxMind file.

--- bin/asngen.py
+++ bin/asngen.py
@@ -15,7 +15,7 @@ import struct
 class ASNGenCommand(GeneratingCommand):
 
     def generate(self):
-                                                      
+
         proxies = {'http': None, 'https': None}
 
         try:
@@ -39,7 +39,7 @@ class ASNGenCommand(GeneratingCommand):
             urllib2.install_opener(opener)
 
         try:
-            url = urllib2.urlopen("https://download.maxmind.com/download/geoip/database/asnum/GeoIPASNum2.zip")
+            url = urllib2.urlopen("https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN-CSV.zip")
         except:
             raise Exception("Please check app proxy settings")
 
@@ -52,72 +52,9 @@ class ASNGenCommand(GeneratingCommand):
             raise Exception("Received response: " + url.getcode())
 
         for name in zipfile.namelist():
-            entries = re.findall(r'^(\d+),(\d+),\"AS(\d+) ([^\"]+)\"', zipfile.open(name).read(), re.MULTILINE)
+            entries = re.findall(r'^(\d+\.\d+\.\d+\.\d+)\/(\d+),(\d+),(.*)', zipfile.open(name).read(), re.MULTILINE)
 
         for line in entries:
-            diff = (int(line[1]) - int(line[0])) -1
-            ip = socket.inet_ntoa(struct.pack('!L', int(line[0])))
-
-            # there's almost certainly a more elegant way of doing this but it works
-            if diff<2**2:
-                mask = 30
-            elif diff<2**3:
-                mask = 29
-            elif diff<2**4:
-                mask = 28
-            elif diff<2**5:
-                mask = 27
-            elif diff<2**6:
-                mask = 26
-            elif diff<2**7:
-                mask = 25
-            elif diff<2**8:
-                mask = 24
-            elif diff<2**9:
-                mask = 23
-            elif diff<2**10:
-                mask = 22
-            elif diff<2**11:
-                mask = 21
-            elif diff<2**12:
-                mask = 20
-            elif diff<2**13:
-                mask = 19
-            elif diff<2**14:
-                mask = 18
-            elif diff<2**15:
-                mask = 17
-            elif diff<2**16:
-                mask = 16
-            elif diff<2**17:
-                mask = 15
-            elif diff<2**18:
-                mask = 14
-            elif diff<2**19:
-                mask = 13
-            elif diff<2**20:
-                mask = 12
-            elif diff<2**21:
-                mask = 11
-            elif diff<2**22:
-                mask = 10
-            elif diff<2**23:
-                mask = 9
-            elif diff<2**24:
-                mask = 8
-            elif diff<2**25:
-                mask = 7
-            elif diff<2**26:
-                mask = 6
-            elif diff<2**27:
-                mask = 5
-            elif diff<2**28:
-                mask = 4
-            elif diff<2**29:
-                mask = 3
-            elif diff<2**30:
-                mask = 2
-
-            yield {'ip': ip + "/" + str(mask), 'start': line[0], 'end': line[1], 'asn': line[2], 'autonomous_system': line[3].decode('utf-8', 'ignore')}
+            yield {'ip': line[0] + "/" + line[1], 'asn': line[2], 'autonomous_system': line[3].decode('utf-8', 'ignore')}
 
 dispatch(ASNGenCommand, sys.argv, sys.stdin, sys.stdout, __name__)
